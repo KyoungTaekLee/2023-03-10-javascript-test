27157c56a17d94c18f6a176557413d3cc9af8a79
LeeKyoungtaek <dlrudxor0302@gmail.com>
2023-03-13 17:59:44 +0900

    디자인, media 빼고 완성

---
+++ source/module.js
@@ -0,0 +1,58 @@
+const _EXAMDATA = {
+  teamInformation: {
+    teamNumber : 0,
+    teamName: "만수 무강",
+    teamNameStory: "무엇보다 건강이 최우선이라는 생각에 우리 모두 아프지 말고 열심히 개발 하며 오래오래 살자는 의미로 '만수무강'이라는 팀명으로 정했습니다.",
+    teamManager: "공욱재",
+    teamAllMember: [
+      {
+        memberNumber: 0,
+        name: "박달재",
+        teamPosition: "팀장",
+        githubAddress: "https://www.github.com/"
+      },
+      {
+        memberNumber: 0,
+        name: "이경택",
+        teamPosition: "팀원",
+        githubAddress: "https://www.github.com/"
+      },
+      {
+        memberNumber: 0,
+        name: "이세민",
+        teamPosition: "팀원",
+        githubAddress: "https://www.github.com/"
+      },
+      {
+        memberNumber: 0,
+        name: "정성철",
+        teamPosition: "팀원",
+        githubAddress: "https://www.github.com/"
+      },
+      {
+        memberNumber: 0,
+        name: "허진",
+        teamPosition: "팀원",
+        githubAddress: "https://www.github.com/"
+      }
+
+    ]
+  },
+  trainingInformation: {
+    subject : [  
+    "웹프로그래밍기본(HTML5&CSS3)",
+    "Git&GitHub",
+    "Babel Javascript",
+    "프레임워크 기초",
+    "Project C(선택)",
+    "백엔드프레임워크런타임",
+    "프레임워크활용",
+    "Project A(데이터 기반 주식 추천 서비스)",
+    "Project B(교통정보제공서비스)",
+    ]
+  },
+  designInformation: {
+    colorSet : ["cadetblue", "salmon", "#ccc", "#333"],
+    breakPoint : [ "768px", "1280px",  "1920px"]
+  }
+}
